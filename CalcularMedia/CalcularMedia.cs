a26f9b5ef435342155c526c6319095c55ce7193d
PedroSantos95 <pedromverasantos@gmail.com>
2018-02-15 19:48:52 +0000

    Cadeiras Ramos

--- CalcularMedia/CalcularMedia.cs
+++ CalcularMedia/CalcularMedia.cs
@@ -12,6 +12,8 @@ namespace CalcularMedia
 {
     public partial class CalcularMedia : Form
     {
+        private int count = 0;
+
         public CalcularMedia()
         {
             InitializeComponent();
@@ -20,7 +22,15 @@ namespace CalcularMedia
         private void carregarCadeirasRamoUtilizador()
         {
             string ramoUtilizador = textBoxRamoUtilizadorAtivo.Text;
-
+            
+            if(count%2 == 0)
+            {
+                ramoUtilizador = "TI";
+            }
+            else
+            {
+                ramoUtilizador = "SI";
+            }
             /*
              * Cadeiras de Tronco Comum
              */
@@ -114,16 +124,83 @@ namespace CalcularMedia
 
             if (ramoUtilizador == "TI")
             {
+                /*
+                 *  Cadeiras do Ramo de Tecnologias de Informação
+                 */
+                
+                //2 Ano 2 Semestre
+                label2_2_3.Text = "Tec. Virtual.: ";
+                label2_2_3.Visible = true;
+                textBox2_2_3.Visible = true;
+                label2_2_4.Text = "Redes Dados:";
+                label2_2_4.Visible = true;
+                textBox2_2_4.Visible = true;
+                label2_2_5.Text = "Admin. Sis.: ";
+                label2_2_5.Visible = true;
+                textBox2_2_5.Visible = true;
+
+                //3 Ano 1 Semestre
+                label3_1_3.Text = "Cent. Dados: ";
+                label3_1_3.Visible = true;
+                textBox3_1_3.Visible = true;
+                label3_1_4.Text = "Top. Redes: ";
+                label3_1_4.Visible = true;
+                textBox3_1_4.Visible = true;
+                label3_1_5.Text = "Seg. Sistemas: ";
+                label3_1_5.Visible = true;
+                textBox3_1_5.Visible = true;
+
+                //3 Ano 2 Semestre
+                label3_2_4.Text = "Lab. Tec. Inf: ";
+                label3_2_4.Visible = true;
+                textBox3_2_4.Visible = true;
+                label3_2_5.Text = "Eng. Sis. Serv.:";
+                label3_2_5.Visible = true;
+                textBox3_2_5.Visible = true;
 
             }
             else
             {
+                /*
+                 *  Cadeiras do Ramo de Sistemas de Informação
+                 */
 
+                //2 Ano 2 Semestre
+                label2_2_3.Text = "Int. Artificial:";
+                label2_2_3.Visible = true;
+                textBox2_2_3.Visible = true;
+                label2_2_4.Text = "Sist. B. Dados:";
+                label2_2_4.Visible = true;
+                textBox2_2_4.Visible = true;
+                label2_2_5.Text = "Seg. Informacao:";
+                label2_2_5.Visible = true;
+                textBox2_2_5.Visible = true;
+
+                //3 Ano 1 Semestre
+                label3_1_3.Text = "Top. Soft.:";
+                label3_1_3.Visible = true;
+                textBox3_1_3.Visible = true;
+                label3_1_4.Text = "DAE:";
+                label3_1_4.Visible = true;
+                textBox3_1_4.Visible = true;
+                label3_1_5.Text = "Sist. A. Dec.:";
+                label3_1_5.Visible = true;
+                textBox3_1_5.Visible = true;
+
+                //3 Ano 2 Semestre
+                label3_2_4.Text = "E. Conhecim.:";
+                label3_2_4.Visible = true;
+                textBox3_2_4.Visible = true;
+                label3_2_5.Text = "Sist. Inf. Emp.:";
+                label3_2_5.Visible = true;
+                textBox3_2_5.Visible = true;
             }
         }
 
         private void buttonCarregarUtilizador_Click(object sender, EventArgs e)
         {
+
+            count++;
             carregarCadeirasRamoUtilizador();
         }
 
